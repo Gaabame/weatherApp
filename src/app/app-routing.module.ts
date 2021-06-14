db516fd46b2f9dc2561db34c89ec2d43b5b556fe
Gabi <gbrielamegiel@gmail.com>
2021-06-14 15:14:54 +0200

    components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {StartComponent} from "./start/start.component";
+import {SelectCityComponent} from "./select-city/select-city.component";
+import {ShowWeatherComponent} from "./show-weather/show-weather.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'start',
+    pathMatch: 'full'
+  },
+  {
+    path: 'start',
+    component: StartComponent
+  },
+  {
+    path: 'select-city',
+    component: SelectCityComponent
+  },
+  {
+    path: 'show-weather',
+    component: ShowWeatherComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartComponent } from './start/start.component';
+import { SelectCityComponent } from './select-city/select-city.component';
+import { ShowWeatherComponent } from './show-weather/show-weather.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    SelectCityComponent,
+    ShowWeatherComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
